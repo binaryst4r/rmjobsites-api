f500ce35f63361b9e78cf460bd9d4d4e15061ec6
Larry Gust <larry.v.gust@gmail.com>
2025-12-19 10:28:02 -0700

    add cors origin for vercel app

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3000", "localhost:5173", "127.0.0.1:3000", "127.0.0.1:5173"
+    origins "localhost:3000", "localhost:5173", "127.0.0.1:3000", "127.0.0.1:5173", "rmjobsites-web.vercel.app"
 
     resource "*",
       headers: :any,
