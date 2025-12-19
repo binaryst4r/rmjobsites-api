68f233783eb0eaa264fd19dc31ce23fe05f17418
Larry Gust <larry.v.gust@gmail.com>
2025-12-19 11:03:37 -0700

    try again

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ require "action_mailer/railtie"
 require "action_mailbox/engine"
 require "action_text/engine"
 require "action_view/railtie"
-require "action_cable/engine"
+# require "action_cable/engine"  # Not needed for API-only app
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
